95449306a074cfb14ee6e7dc40b1184b207b24a5
Adam Ruzicka <aruzicka@redhat.com>
2023-08-04 09:41:48 +0000

    AST

--- lib/lispir.rb
+++ lib/lispir.rb
@@ -3,6 +3,7 @@ require "lispir/version"
 module Lispir
   require 'lispir/tokenizer'
   require 'lispir/value'
+  require 'lispir/ast'
   class Error < StandardError; end
   # Your code goes here...
 end
---
+++ lib/lispir/ast.rb
@@ -0,0 +1,34 @@
+module Lispir
+  class AST
+    attr_reader :ast
+    def initialize(tokens)
+      @ast = to_ast(tokens)
+    end
+
+    private
+
+    def to_ast(tokens)
+      stack = []
+      ast = []
+      tokens.each do |token|
+        case token
+        when '('
+          stack << ast
+          ast = []
+        when ')'
+          completed = ast
+          ast = stack.pop
+          ast << Lispir::Value::List.new(completed)
+        else
+          if token =~ /^\d+$/
+            ast << Lispir::Value::Number.new(token.to_i)
+          else
+            ast << Lispir::Value::Atom.new(token)
+          end
+        end
+      end
+
+      ast
+    end
+  end
+end
